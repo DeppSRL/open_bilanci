61c4dc06fc91078afd8e2ec6bf63bbf8608954e8
verganis <stefano.vergani.it@gmail.com>
2013-12-04 15:15:46 +0100

    Added scrapy and scraper_project

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ open_bilanci
 ============
 
 Open Bilanci Project
+
+* contains the application of Open Bilanci
+
+Scraper Project
+
+* contains the code for the scraper that gets Bilanci data from Ministero dell'Interno
---
+++ scraper_project/scraper_project/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for scraper_project project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scraper_project'
+
+SPIDER_MODULES = ['scraper_project.spiders']
+NEWSPIDER_MODULE = 'scraper_project.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scraper_project (+http://www.yourdomain.com)'
