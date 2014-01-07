96c3d42a5280d7be45b354af425b8e67ebb890d4
Depp <stefano.vergani.it@gmail.com>
2014-01-07 15:38:45 +0100

    Map reduce function for Q4 consuntivo Ok

--- couchdb_scripts/getkeys.py
+++ couchdb_scripts/getkeys.py
@@ -10,55 +10,58 @@ db = server['bilanci_raw']
 
 
 def quadro4_getkeys(doc):
-    all_keys = []
-    possible_values =['quadro-4-a-impegni','quadro-4-a-impegni-1']
-    for value in possible_values:
-        if value in doc['consuntivo']['04'].keys():
-            for key in doc['consuntivo']['04'][value]['data'].keys():
-                clean_key = key.lower().strip()
-                # se presente toglie il carattere - iniziale
-                try:
-                    if clean_key.index('- ') == 0:
-                        clean_key = clean_key[2:]
-                except ValueError:
-                    pass
+    all_keys = {
+        'a':[],
+        'b':[],
+        'c':[]
+    }
 
-                all_keys.append(clean_key)
+    possible_values = {
+        'a':['quadro-4-a-impegni','quadro-4-a-impegni-1'],
+        'b':['quadro-4-b-pagamenti-in-conto-competenza','quadro-4-b-pagamenti-in-conto-competenza-1'],
+        'c':['quadro-4-c-pagamenti-in-conto-residui','quadro-4-c-pagamenti-in-conto-residui-1']
+    }
 
+    for titolo_key,titolo_values in possible_values.iteritems():
+        for titolo_name in titolo_values:
+            if titolo_name in doc['consuntivo']['04'].keys():
+                for sottovoce in doc['consuntivo']['04'][titolo_name]['data'].keys():
+                    clean_sottovoce = sottovoce.lower().strip()
+                    # se presente toglie il carattere - iniziale
+                    try:
+                        if clean_sottovoce.index('- ') == 0:
+                            clean_sottovoce = clean_sottovoce[2:]
+                    except ValueError:
+                        pass
 
-    if len(all_keys) < 1:
-        if 'consuntivo' in doc.keys():
-            all_keys.append('CONSUNTIVO NOT AVAILABLE')
-        else:
-            all_keys.append('QUADRO IMPEGNI NOT AVAILABLE')
+                    all_keys[titolo_key].append(clean_sottovoce)
 
-    yield ('x', all_keys)
-
-
-def quadro4_reduce(keys,values,rereduce):
-    total= []
-
-    for value in values:
-        for key in value:
-            if key not in total:
-                total.append(key)
-    return sorted(total)
+    all_keys['a']=sorted(all_keys['a'])
+    all_keys['b']=sorted(all_keys['b'])
+    all_keys['c']=sorted(all_keys['c'])
+    yield ('key', all_keys)
 
 
-def map_keys(doc):
-    if len(doc['value']['preventivo']['04'].keys())>0:
-        yield (doc['value']['preventivo']['04'].keys()[0],1)
-    else:
-        yield ('*',1)
+def keys_reduce(keys,values,rereduce):
+    total={'a':[],'b':[],'c':[]}
 
-def summingReducer(keys, values, rereduce):
-    return sum(values)
+    all_keys_list = values
+    for all_key in all_keys_list:
+        for titolo_name in all_key.keys():
+            for voce in all_key[titolo_name]:
+                if voce not in total[titolo_name]:
+                    total[titolo_name].append(voce)
 
+    # ordina alfabeticamente i risultati
+    for titolo_name in total.keys():
+        total[titolo_name]=sorted(total[titolo_name])
+
+    return total
 
 
 # sync the view
-view = ViewDefinition('tree_getkeys', 'quadro4_getkeys', map_fun=quadro4_getkeys, reduce_fun=quadro4_reduce, language='python')
-pprint(view.sync(db))
+view = ViewDefinition('tree_getkeys', 'quadro4_getkeys', map_fun=quadro4_getkeys, reduce_fun=keys_reduce, language='python')
+view.sync(db)
 
 
 
@@ -69,8 +72,6 @@ c = 0
 
 for row in db.view('tree_getkeys/quadro4_getkeys'):
     docs.append(row)
-    c += 1
-    if c > 100:
-        break
+
 
 print json.dumps(docs, indent=4)
