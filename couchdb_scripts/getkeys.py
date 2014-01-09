f93309f592a07797ade7669015845c5089777513
Depp <stefano.vergani.it@gmail.com>
2014-01-09 18:29:05 +0100

    Added voci mapping function, fixed bug

--- couchdb_scripts/getkeys.py
+++ couchdb_scripts/getkeys.py
@@ -42,10 +42,10 @@ def quadro4_getkeys(doc):
 def titoli_getkeys(doc):
     # funzione che raccoglie per tutti i bilanci tutti i nomi
     # dei titoli, quadro per quadro
-    ignored_keys =["_rev", "_id"]
+    considered_keys =["consuntivo", "preventivo"]
     if doc:
         for doc_keys in doc.keys():
-            if doc_keys not in ignored_keys:
+            if doc_keys in considered_keys:
                 tipo_bilancio = doc_keys
                 for quadro_n, quadro_v  in doc[tipo_bilancio].iteritems():
 
@@ -53,6 +53,35 @@ def titoli_getkeys(doc):
                     # il valore 1 ci permette di fare somme con la reduce function _sum()
                     for nome_titolo in quadro_v.keys():
                         yield ([tipo_bilancio,quadro_n,nome_titolo,doc['_id'][:4]],1)
+
+
+def translate_titolo(nome_titolo):
+    # funzione che traduce il nome titolo in ingresso nel corrispondente nome titolo
+    # dell'albero semplificato
+
+    return nome_titolo
+
+
+def voci_getkeys(doc):
+    # funzione che raccoglie per tutti i bilanci tutti i nomi
+    # delle voci, titolo per titolo, quadro per quadro
+    considered_keys =["consuntivo", "preventivo"]
+    if doc:
+        for document_keys in doc.keys():
+            if document_keys in considered_keys:
+                tipo_bilancio = document_keys
+                for quadro_n, quadro_v  in doc[tipo_bilancio].iteritems():
+
+                    # genera una chiave che contiene tipo di bilancio, quadro e la voce
+                    # il valore 1 ci permette di fare somme con la reduce function _sum()
+                    for nome_titolo, contenuto in quadro_v.iteritems():
+
+                        if 'data' in contenuto.keys():
+                            if len(contenuto['data'])>0:
+                                for voce in contenuto['data']:
+                                    anno = doc['_id'][:4]
+                                    yield ([tipo_bilancio,quadro_n,nome_titolo,voce,anno],1)
+
 
 
 
@@ -79,10 +108,19 @@ def main(argv):
         },
     }
 
+    accepted_views = {
+        'voci':voci_getkeys,
+        'titoli': titoli_getkeys
+    }
+
 
     parser.add_argument('--server','-s', dest='server_name', action='store',
-                   default='localhost',
-                   help='Server name: localhost | staging')
+               default='localhost',
+               help='Server name: localhost | staging')
+
+    parser.add_argument('--function','-f', dest='function', action='store',
+               default='voci',
+               help='Function to sync: titoli | voci')
 
     parser.add_argument("--check-function","-ck", help="check function after synch",
                     action="store_true")
@@ -90,6 +128,7 @@ def main(argv):
     args = parser.parse_args()
     server_name= args.server_name
     check_function= args.check_function
+    function_to_sync = args.function
 
     if server_name and check_function:
         # costruisce la stringa per la connessione al server aggiungendo user/passw se necessario
@@ -101,19 +140,31 @@ def main(argv):
 
         server_string+=accepted_servers[server_name]['host']+":"+accepted_servers[server_name]['port']
 
-        print "Connecting to:"+server_string
+        print "Connecting to: "+server_string+" ..."
         # open db connection
         server = couchdb.Server(server_string)
         db = server[accepted_servers[server_name]['db_name']]
-        # sync the view
-        view = ViewDefinition('tree_getkeys', 'cons_titoli_getkeys', map_fun=titoli_getkeys, reduce_fun='_sum()', language='python')
-        view.sync(db)
+        print "Done!"
 
-        if check_function:
-            # get view values
-            check = db.view('tree_getkeys/cons_titoli_getkeys')
+        if function_to_sync in accepted_views:
+            view_name = function_to_sync
+            reduce_function = accepted_views[view_name]
 
+            # sync the view
+            view = ViewDefinition('tree_getkeys',view_name, map_fun=reduce_function, reduce_fun='_sum()', language='python')
+            view.sync(db)
 
+            if check_function:
+                # get view values
+                check = db.view('tree_getkeys/'+view_name)
+                # dummy code just to test the function
+                for row in check:
+                    print "Test output:"
+                    pprint(row)
+                    break
+
+        else:
+            print "Function "+function_to_sync+" not accepted"
 
 
 # launches main function
