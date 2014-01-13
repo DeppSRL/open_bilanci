98c4f6eb94dfa39f60c23718f7464767f6a16ef7
Depp <stefano.vergani.it@gmail.com>
2014-01-13 20:10:46 +0100

    Translate keys: added creation / deletion of destination db

--- couchdb_scripts/translate_keys.py
+++ couchdb_scripts/translate_keys.py
@@ -10,9 +10,6 @@ from settings_local import *
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Translate bilanci keys, copying elements from a db to a new one')
-    server_name = None
-    check_function= None
-
 
     accepted_servers = {
         'localhost': {
@@ -34,14 +31,7 @@ def main(argv):
         },
     }
 
-
-    accepted_functions = {
-        'titoli':{
-            'gdoc_url':'',
-
-        }
-    }
-
+    translation_map = {}
 
     parser.add_argument('--server','-s', dest='server_name', action='store',
                default='localhost',
@@ -59,25 +49,7 @@ def main(argv):
     create_db= args.create_db
     copy_design_docs = args.copy_design
 
-
     if server_name in accepted_servers.keys():
-        # costruisce la stringa per la connessione al server aggiungendo user/passw se necessario
-        server_string ='http://'
-        if accepted_servers[server_name]['user']:
-            server_string+=accepted_servers[server_name]['user']+":"
-            if accepted_servers[server_name]['password']:
-                server_string+=accepted_servers[server_name]['password']+"@"
-
-        server_string+=accepted_servers[server_name]['host']+":"+accepted_servers[server_name]['port']
-
-        print "Connecting to: "+server_string+" ..."
-        # open db connection
-        server = couchdb.Server(server_string)
-        source_db = server[accepted_servers[server_name]['source_db_name']]
-        # destination_db = server[accepted_servers[server_name]['destination_db_name']]
-        print "Db connection ok!"
-
-
         # Login with the script Google account
         gc = gspread.login(g_user, g_password)
 
@@ -91,12 +63,58 @@ def main(argv):
 
 
         # Select worksheet by index. Worksheet indexes start from zero
-        list_worksheet = list_sheet.get_worksheet(0)
-        list_values = list_worksheet.get_all_values()
+        titoli_ws_values = list_sheet.get_worksheet(0).get_all_values()
 
+
+        for row in titoli_ws_values[1:]:
+            # considero valide solo le righe che hanno l'ultimo valore (titolo di destinazione) non nullo
+            if row[3]:
+
+                if row[0] not in translation_map:
+                    translation_map[row[0]] = {}
+                if row[1] not in translation_map[row[0]]:
+                    translation_map[row[0]][row[1]] = {}
+                if row[2] not in translation_map[row[0]][row[1]]:
+                    translation_map[row[0]][row[1]][row[2]] = {}
+
+                #  crea la mappa di conversione dei titoli
+                translation_map[row[0]][row[1]][row[2]]=row[3]
+
+
+        # connessione a couchdb
+        # costruisce la stringa per la connessione al server aggiungendo user/passw se necessario
+        server_string ='http://'
+        if accepted_servers[server_name]['user']:
+            server_string+=accepted_servers[server_name]['user']+":"
+            if accepted_servers[server_name]['password']:
+                server_string+=accepted_servers[server_name]['password']+"@"
+
+        server_string+=accepted_servers[server_name]['host']+":"+accepted_servers[server_name]['port']
+
+        print "Connecting to: "+server_string+" ..."
+        # open db connection
+        server = couchdb.Server(server_string)
+        source_db = server[accepted_servers[server_name]['source_db_name']]
+        print "Source DB connection ok!"
+
+        if create_db:
+            # se esiste il db lo cancella
+            if accepted_servers[server_name]['destination_db_name'] in server:
+                del server[accepted_servers[server_name]['destination_db_name']]
+                print  "Destination db: "+  accepted_servers[server_name]['destination_db_name'] +" deleted"
+            # crea il db
+            destination_db = server.create(accepted_servers[server_name]['destination_db_name'])
+            print  "Destination db: "+  accepted_servers[server_name]['destination_db_name'] +" created"
+        else:
+            #     apre una connessione con destination db
+            try:
+                destination_db = server[accepted_servers[server_name]['destination_db_name']]
+            except couchdb.http.ResourceNotFound:
+                print "Destination db: "+  accepted_servers[server_name]['destination_db_name'] +" not found"
+                return
 
     else:
-        print "no op:"+server_name+","+str(check_function)
+        print "server not accepted:"+server_name
 
 
 # launches main function
