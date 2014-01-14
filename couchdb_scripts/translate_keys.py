a6231796cd2bcc63c533124cfe3828932f25c27a
Depp <stefano.vergani.it@gmail.com>
2014-01-14 16:42:26 +0100

    Translate key: now ok, accepting localhost and staging server

--- couchdb_scripts/translate_keys.py
+++ couchdb_scripts/translate_keys.py
@@ -5,6 +5,7 @@ import gspread
 import argparse
 from couchdb.design import ViewDefinition
 from pprint import pprint
+import requests
 from settings_local import *
 
 
@@ -40,14 +41,10 @@ def main(argv):
     parser.add_argument("--create-db","-create", help="create new destination db",
                     action="store_true")
 
-    parser.add_argument("--copy-design","-cpdd", help="include design documents in the copy process",
-                    action="store_true")
-
     args = parser.parse_args()
     
     server_name= args.server_name
     create_db= args.create_db
-    copy_design_docs = args.copy_design
 
     if server_name in accepted_servers.keys():
         # Login with the script Google account
@@ -69,31 +66,34 @@ def main(argv):
         for row in titoli_ws_values[1:]:
             # considero valide solo le righe che hanno l'ultimo valore (titolo di destinazione) non nullo
             if row[3]:
+                # zero padding per n_quadro: '2' -> '02'
+                n_quadro=row[1].zfill(2)
 
                 if row[0] not in translation_map:
                     translation_map[row[0]] = {}
-                if row[1] not in translation_map[row[0]]:
-                    translation_map[row[0]][row[1]] = {}
-                if row[2] not in translation_map[row[0]][row[1]]:
-                    translation_map[row[0]][row[1]][row[2]] = {}
+                if n_quadro not in translation_map[row[0]]:
+                    translation_map[row[0]][n_quadro] = {}
+                if row[2] not in translation_map[row[0]][n_quadro]:
+                    translation_map[row[0]][n_quadro][row[2]] = {}
 
                 #  crea la mappa di conversione dei titoli
-                translation_map[row[0]][row[1]][row[2]]=row[3]
+                # la chiave e' tipo_bilancio, numero_quadro , nome_titolo
+                translation_map[row[0]][n_quadro][row[2]]=row[3]
 
 
         # connessione a couchdb
         # costruisce la stringa per la connessione al server aggiungendo user/passw se necessario
-        server_string ='http://'
+        server_connection_address ='http://'
         if accepted_servers[server_name]['user']:
-            server_string+=accepted_servers[server_name]['user']+":"
+            server_connection_address+=accepted_servers[server_name]['user']+":"
             if accepted_servers[server_name]['password']:
-                server_string+=accepted_servers[server_name]['password']+"@"
+                server_connection_address+=accepted_servers[server_name]['password']+"@"
 
-        server_string+=accepted_servers[server_name]['host']+":"+accepted_servers[server_name]['port']
+        server_connection_address+=accepted_servers[server_name]['host']+":"+accepted_servers[server_name]['port']
 
-        print "Connecting to: "+server_string+" ..."
+        print "Connecting to: "+server_connection_address+" ..."
         # open db connection
-        server = couchdb.Server(server_string)
+        server = couchdb.Server(server_connection_address)
         source_db = server[accepted_servers[server_name]['source_db_name']]
         print "Source DB connection ok!"
 
@@ -112,6 +112,62 @@ def main(argv):
             except couchdb.http.ResourceNotFound:
                 print "Destination db: "+  accepted_servers[server_name]['destination_db_name'] +" not found"
                 return
+
+
+
+        # legge la lista di id per recuperare tutti gli oggetti del db
+        get_all_docs_url = server_connection_address+'/'+accepted_servers[server_name]['source_db_name']+'/_all_docs?include_docs=false'
+
+        id_list_response = requests.get(get_all_docs_url ).json()
+
+        if 'rows' in id_list_response.keys():
+            id_list=id_list_response['rows']
+
+            for row in id_list:
+                source_document = source_db.get(row['id'])
+                print "Analyzing doc_id:"+row['id']
+
+                if source_document is not None:
+                    destination_document = {'_id': row['id']}
+
+                    #  per ogni tipo di bilancio
+                    for bilancio_name in ['preventivo','consuntivo']:
+                        if bilancio_name in source_document.keys():
+                            bilancio_object = source_document[bilancio_name]
+                            destination_document[bilancio_name]={}
+
+                            for quadro_name, quadro_object in bilancio_object.iteritems():
+                                destination_document[bilancio_name][quadro_name]={}
+                                for titolo_name, titolo_object in quadro_object.iteritems():
+                                    # per ogni titolo presente, se il titolo e' nella translation map
+                                    # applica la trasformazione, poi copia il contenuto nell'oggetto di destinazione
+
+                                    if titolo_name in translation_map[bilancio_name][quadro_name].keys():
+                                        titolo_name_translated = translation_map[bilancio_name][quadro_name][titolo_name]
+                                    else:
+                                        titolo_name_translated = titolo_name
+
+                                    # crea il dizionario con il nome tradotto
+                                    destination_document[bilancio_name][quadro_name][titolo_name_translated]={}
+                                    # crea i meta
+                                    if 'meta' in titolo_object.keys():
+                                        destination_document[bilancio_name][quadro_name][titolo_name_translated]['meta']={}
+                                        destination_document[bilancio_name][quadro_name][titolo_name_translated]['meta']=titolo_object['meta']
+
+                                    # passa i dati sul nuovo oggetto
+                                    if 'data' in titolo_object.keys():
+                                        destination_document[bilancio_name][quadro_name][titolo_name_translated]['data']={}
+                                        destination_document[bilancio_name][quadro_name][titolo_name_translated]['data'] =\
+                                                    titolo_object['data']
+
+
+                    # controlla che alcune voci di titoli non siano andate perse nella traduzione
+                    if bilancio_name in source_document:
+                        if len(destination_document[bilancio_name][quadro_name].keys()) != len(source_document[bilancio_name][quadro_name].keys()):
+                            print "Error: Different number of keys for doc_id:"+row['id']
+
+                    # scrive il nuovo oggetto nel db di destinazione
+                    destination_db.save(destination_document)
 
     else:
         print "server not accepted:"+server_name
