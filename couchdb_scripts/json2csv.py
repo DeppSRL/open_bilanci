6a37505532151e4ffe10c40dd3da1649061da9e9
Depp <stefano.vergani.it@gmail.com>
2014-01-15 19:01:23 +0100

    Json2csv now accepts shell parameters

--- couchdb_scripts/json2csv.py
+++ couchdb_scripts/json2csv.py
@@ -1,24 +1,61 @@
-__author__ = 'stefano'
 
+import sys
+import argparse
 import csv
 import json
+import utils
 
 
-# questo script prende i risultati del design document titoli getkeys e genera un file CSV
-json_filename = 'output/staging_cons_titoli_getkeys.json'
+def main(argv):
+    parser = argparse.ArgumentParser(description='Convert json file to csv for titoli and voci')
 
-with open(json_filename, 'r') as content_file:
-    json_file_content = content_file.read()
 
-json_data = json.loads(json_file_content)
+    parser.add_argument('--filename','-f', dest='filename', action='store',
+               default='',
+               help='Insert json file name')
 
-f = csv.writer(open("output/bilanci_quadri_titoli_map.csv", "wb+"))
+    parser.add_argument('--type','-t', dest='type', action='store',
+               default='voci',
+               help='Type to convert: titoli | voci')
 
-# Write CSV Header, If you dont need that, remove this line
-f.writerow(["tipo_bilancio", "quadro", "titolo",])
 
-for row in json_data['rows']:
-    f.writerow([row["key"][0],
-                row["key"][1],
-                row["key"][2],
-                ])
+    accepted_types={
+        'voci':{
+            'keys':["tipo_quadro_titolo", "voce",]
+        },
+        'titoli':{
+            'keys':["tipo_quadro", "titolo",]
+        }
+    }
+
+    args = parser.parse_args()
+    json_filename= args.filename
+    # todo fare anche la versione per i titoli
+    type= args.type
+
+
+    if type in accepted_types.keys():
+
+        # apre il file json
+        with open(json_filename, 'r') as content_file:
+            json_file_content = content_file.read()
+
+        json_data = json.loads(json_file_content)
+        csv_file = open(json_filename.replace('json', 'csv'), "wb+")
+
+        udw = utils.UnicodeDictWriter(csv_file, ["tipo_quadro_titolo", "voce",], dialect=csv.excel, encoding="utf-8")
+
+        for row in json_data['rows']:
+
+            mydict = { "tipo_quadro_titolo": row['key'][0], "voce": row['key'][1] }
+            udw.writerow(mydict)
+    else:
+        print "Error: Type "+type+" not accepted"
+
+
+
+
+
+# launches main function
+if __name__ == "__main__":
+   main(sys.argv[1:])
