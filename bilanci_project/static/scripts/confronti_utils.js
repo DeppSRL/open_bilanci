e6a51b12734c59153e1ed3216dcc5b4cef433a2d
Depp <stefano.vergani.it@gmail.com>
2014-03-21 16:46:38 +0100

    Confronti: added different cluster warning msg

--- bilanci_project/static/scripts/confronti_utils.js
+++ bilanci_project/static/scripts/confronti_utils.js
@@ -2,6 +2,7 @@
  * Created by stefano on 3/21/14.
  */
 var url_is_correct = false;
+var error_box_visible = false;
 
 function setIndicatorLinksUrl(){
     var territorio_1_slug = $("#id_territorio_1").select2("val");
@@ -33,7 +34,43 @@ function setSubmitButtonUrl(){
     $("#confronti_submit_btn").attr("href", url);
 }
 
-function changeConfrontiDataUrl(){
+function showWarningBox(msg){
+    //insert error text
+    $( "#error_msg_box_txt" ).html(msg);
+    //show error_msg_box
+    $("#error_msg_box").removeClass('hidden');
+    error_box_visible = true;
+}
+
+function hideWarningBox(){
+    if(error_box_visible){
+        $("#error_msg_box").addClass('hidden');
+        $( "#error_msg_box_txt" ).html('');
+    }
+
+}
+
+function checkTerritoriCluster(){
+    var needle = 'Cluster:';
+
+    var territorio_1_name = $("#id_territorio_1").select2('data').text;
+    var territorio_2_name = $("#id_territorio_2").select2('data').text;
+
+    var territorio_1_cluster = territorio_1_name.substr(territorio_1_name.indexOf(needle) + needle.length);
+    var territorio_2_cluster = territorio_2_name.substr(territorio_2_name.indexOf(needle) + needle.length);
+
+    if(territorio_1_cluster != territorio_2_cluster){
+        // if territori are in different cluster show the error box
+        showWarningBox('ATTENZIONE, HAI SCELTO DUE COMUNI CHE APPARTENGONO A CLASSI DIMENSIONALI DIVERSE IL CONFRONTO POTREBBE NON AVERE SIGNIFICATO');
+
+    }
+    else{
+        // if territori are in the same cluster hide the error box
+        hideWarningBox();
+    }
+}
+
+function changeConfrontiUrl(){
 
     var territorio_1_slug = $("#id_territorio_1").select2("val");
     var territorio_2_slug = $("#id_territorio_2").select2("val");
@@ -46,6 +83,9 @@ function changeConfrontiDataUrl(){
         // sets url for indicatori link in the side menu
         setIndicatorLinksUrl();
 
+        //if the territori selected are in different cluster show warning msg
+        checkTerritoriCluster();
+
         url_is_correct = true;
     }
     else{
@@ -54,23 +94,6 @@ function changeConfrontiDataUrl(){
     }
 }
 
-
-function changeConfrontiHomeUrl(){
-            var territorio_1_slug = $("#id_territorio_1").select2("val");
-            var territorio_2_slug = $("#id_territorio_2").select2("val");
-
-            if(territorio_1_slug && territorio_2_slug){
-
-                //sets url for the fake submit button
-                var url = '/confronti/'+territorio_1_slug+"/"+territorio_2_slug+"";
-                $("#confronti_submit_btn").attr("href", url);
-                url_is_correct = true;
-            }
-            else{
-
-                url_is_correct = false;
-            }
-        }
 
 function submitButtonConfronti(e){
      e.preventDefault();
@@ -82,10 +105,7 @@ function submitButtonConfronti(e){
         window.location = $("#confronti_submit_btn").attr('href');
     }
     else{
-        //insert error text
-        $( "#error_msg_box_txt" ).html('ATTENZIONE, DEVI SELEZIONARE DUE COMUNI PER PROCEDERE NEL CONFRONTO')
-        //show error_msg_box
-        $("#error_msg_box").removeClass('hidden');
+        showWarningBox('ATTENZIONE, DEVI SELEZIONARE DUE COMUNI PER PROCEDERE NEL CONFRONTO');
 
     }
 }
