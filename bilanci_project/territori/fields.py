a5cd0d3b888842564780e8cfced3871c32a013ae
Depp <stefano.vergani.it@gmail.com>
2014-05-10 11:18:00 +0200

    Territori field search: updated select

--- bilanci_project/territori/fields.py
+++ bilanci_project/territori/fields.py
@@ -1,13 +1,21 @@
 from django.conf import settings
+from bilanci.models import ValoreBilancio
 from territori.models import Territorio, Contesto
 from django_select2.fields import AutoModelSelect2Field
 
 class TerritoriChoices(AutoModelSelect2Field):
+    #
+    # queryset = Territorio.objects.\
+    #             filter(territorio=Territorio.TERRITORIO.C).\
+    #             exclude( valorebilancio__isnull=True).\
+    #             order_by('-cluster', 'denominazione')
 
-    queryset = Territorio.objects.\
-                filter(territorio=Territorio.TERRITORIO.C).\
-                exclude( valorebilancio__isnull=True).\
-                order_by('-cluster', 'denominazione')
+    vb = ValoreBilancio.objects.filter(territorio__territorio=Territorio.TERRITORIO.C).\
+        filter(anno=2013,voce__slug='preventivo').values_list('territorio__pk',flat=True)
+
+    queryset =  Territorio.objects.filter(pk__in=vb).\
+        order_by('-cluster','denominazione')
+
 
     search_fields = ['denominazione__icontains', ]
 
