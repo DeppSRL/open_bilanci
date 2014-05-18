e5a48aebc17abcf184d1c9b2b861556ae2faf8ec
Guglielmo Celata <guglielmo.celata@gmail.com>
2014-05-19 00:11:25 +0400

    LoginRequiredMixin removed

--- bilanci_project/bilanci/views.py
+++ bilanci_project/bilanci/views.py
@@ -58,7 +58,7 @@ class ShareUrlMixin(object):
         return super(ShareUrlMixin, self).get(request, *args, **kwargs)
 
 
-class HomeView(LoginRequiredMixin, TemplateView):
+class HomeView(TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
