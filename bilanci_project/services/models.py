892defb084202728ce5ffd90f13606fa3672eb9e
verganis <stefano.vergani.it@gmail.com>
2014-09-16 18:27:14 +0200

    Servizi ai Comuni: initial model commit

---
+++ bilanci_project/services/models.py
@@ -0,0 +1,13 @@
+__author__ = 'stefano'
+# -*- coding: utf-8 -*-
+from django.db import models
+from territori.models import Territorio
+
+class PaginaComune(models.Model):
+    
+    base_url = models.URLField(blank=False, null=False, default=0)
+    territorio = models.ForeignKey(Territorio, null=False, blank=False, db_index=True)
+    header_text = models.TextField()
+    footer_text = models.TextField()
+    logo = models.FilePathField()
+    active = models.BooleanField(default=False)
