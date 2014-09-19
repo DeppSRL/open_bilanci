5bc4f8b620e234d60615d0a23b91247a1def8f98
verganis <stefano.vergani.it@gmail.com>
2014-09-19 12:53:16 +0200

    Settings staging: added host comuni list

--- bilanci_project/bilanci/settings/staging.py
+++ bilanci_project/bilanci/settings/staging.py
@@ -4,7 +4,22 @@ from base import *
 
 ########## HOST CONFIGURATION
 # See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
-ALLOWED_HOSTS = ['*']
+########## HOST CONFIGURATION
+# See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
+ALLOWED_HOSTS = [
+    'www.openbilanci.it',
+    'openbilanci.staging.deppsviluppo.org'
+    ]
+
+
+# Allowed hosts expansion: needed for servizi ai Comuni
+
+HOSTS_COMUNI = [
+    
+    ]
+
+ALLOWED_HOSTS += HOSTS_COMUNI
+
 ########## END HOST CONFIGURATION
 
 ########## EMAIL CONFIGURATION
