1a9c88c7d914b01a74bb59f94ac1a3be5b527993
verganis <stefano.vergani.it@gmail.com>
2014-09-26 15:55:23 +0200

    Services: header/footer are now html text

--- bilanci_project/services/models.py
+++ bilanci_project/services/models.py
@@ -1,3 +1,6 @@
+from tinymce.models import HTMLField
+from django.utils.translation import ugettext_lazy as _
+
 __author__ = 'stefano'
 # -*- coding: utf-8 -*-
 from django.db import models
@@ -8,8 +11,8 @@ class PaginaComune(models.Model):
     host = models.TextField(blank=False, null=False, default='')
     backlink = models.URLField(blank=False, null=False, default='')
     territorio = models.ForeignKey(Territorio, null=False, blank=False, db_index=True)
-    header_text = models.TextField()
-    footer_text = models.TextField()
+    header_text = HTMLField(_("Header text"), help_text=_("Testo che appare nella testata"), blank=True)
+    footer_text = HTMLField(_("Footer text"), help_text=_("Testo che appare nel fondo pagina"), blank=True)
     logo = models.ImageField(null=True, blank=True, upload_to='comune_logo/')
     active = models.BooleanField(default=False)
 
