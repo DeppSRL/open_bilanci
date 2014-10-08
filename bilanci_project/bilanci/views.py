b1cdbacff3aa4a7aab65388e19faba1ee8a4950b
verganis <stefano.vergani.it@gmail.com>
2014-10-08 16:08:10 +0200

    Views:Fixed bug over incarichi

--- bilanci_project/bilanci/views.py
+++ bilanci_project/bilanci/views.py
@@ -262,15 +262,13 @@ class IncarichiGetterMixin(object):
             timeline_start_date = settings.TIMELINE_START_DATE.date()
             timeline_end_date = settings.TIMELINE_END_DATE.date()
 
+            dict_widget['start'] = incarico.data_inizio.strftime(self.date_fmt)
+            dict_widget['end'] = incarico.data_fine.strftime(self.date_fmt)
+
             if incarico.data_inizio < timeline_start_date:
                 dict_widget['start'] = timeline_start_date.strftime(self.date_fmt)
-            else:
-                dict_widget['start'] = incarico.data_inizio.strftime(self.date_fmt)
 
-
-            if incarico.data_fine:
-                dict_widget['end'] = incarico.data_fine.strftime(self.date_fmt)
-            else:
+            if incarico.data_fine > timeline_end_date:
                 dict_widget['end'] = timeline_end_date.strftime(self.date_fmt)
 
             if incarico.pic_url:
