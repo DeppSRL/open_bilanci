7a8131f6317bb1cb307ff2274b6b6f9a039bfd3b
verganis <stefano.vergani.it@gmail.com>
2014-12-19 11:55:00 +0100

    Update staging server ip

--- bilanci_project/bilanci/settings/base.py
+++ bilanci_project/bilanci/settings/base.py
@@ -351,10 +351,10 @@ COUCHDB_SERVERS = {
         'password':env('COUCHDB_LOCALHOST_PASSWORD'),
     },
     'staging': {
-        'host': 'staging.depp.it',
-        'port': '5984',
+        'host': env('COUCHDB_STAGING_HOST'),
+        'port': env('COUCHDB_STAGING_PORT'),
         'user': env('COUCHDB_STAGING_USER'),
-        'password':env('COUCHDB_STAGING_PASSWORD'),
+        'password': env('COUCHDB_STAGING_PASSWORD'),
     },
 }
 COUCHDB_DEFAULT_SERVER = 'staging'
