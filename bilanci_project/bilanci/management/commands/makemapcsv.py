c2ae92b54aaac97cc41ec1db834af25aabf305ec
Guglielmo <guglielmo.celata@gmail.com>
2015-10-01 14:48:29 +0200

    comments added

--- bilanci_project/bilanci/management/commands/makemapcsv.py
+++ bilanci_project/bilanci/management/commands/makemapcsv.py
@@ -55,6 +55,7 @@ class Command(BaseCommand):
     comuni_dicts = {}
 
     def handle(self, *args, **options):
+        # sets log level, according to verbosity
         verbosity = options['verbosity']
         if verbosity == '0':
             self.logger.setLevel(logging.ERROR)
@@ -65,35 +66,35 @@ class Command(BaseCommand):
         elif verbosity == '3':
             self.logger.setLevel(logging.DEBUG)
 
-        ###
         # year
-        ###
-        year = options['year']
-        if not year:
+        self.year = options['year']
+        if not self.year:
             raise Exception("Missing year parameter")
+        self.logger.info("Processing year: {0}".format(self.year))
 
         # site_url
         site_url = options['site_url']
 
-        self.year = year
-        self.logger.info("Processing year: {0}".format(self.year))
-
+        # selected indicatori
         indicators_slugs = options['indicators'].split(',')
         indicatori = Indicatore.objects.filter(slug__in=indicators_slugs)
 
+        # selected voices
         voices_slugs = options['voices'].split(',')
         voices = Voce.objects.filter(slug__in=voices_slugs)
 
+        # global variable containing all values
         indicatori_voci_year = {}
 
+        # values for selected indicatori
         self.logger.debug("reading indicatori values")
         for i in indicatori:
-            # extracts indicator values for all locations
             self.logger.debug(
                 u" processing indicator: {0}".format(i.denominazione)
             )
+            # extracts indicator values for all locations
             iv = ValoreIndicatore.objects.filter(
-                indicatore__slug=i.slug, anno=year
+                indicatore__slug=i.slug, anno=self.year
             ).values(
                 'territorio__istat_id', 'territorio__slug', 'valore'
             )
@@ -101,20 +102,29 @@ class Command(BaseCommand):
                 t_id = v['territorio__istat_id']
                 t_slug = v['territorio__slug']
                 t_url = "{0}/bilanci/{1}?year={2}".format(
-                    site_url, t_slug, year
+                    site_url, t_slug, self.year
                 )
                 val = v['valore']
+
+                # creates location record if not found
+                # with URL and empty values dictionary
                 if t_id not in indicatori_voci_year:
                     indicatori_voci_year[t_id] = {'url': t_url, 'values': {}}
+
+                # adds value for indicator
+                # indicator ID is prefixed with _i
+                # to avoid clashes with voices values
                 indicatori_voci_year[t_id]['values']['i_' + str(i.id)] = val
 
+        # values (precapite) from budget voices
         self.logger.debug("adding voci values")
         for vo in voices:
             self.logger.debug(
                 u" processing voice: {0}".format(vo.denominazione)
             )
+            # extracts voice procapite values for all locations
             vv = ValoreBilancio.objects.filter(
-                voce__slug=vo.slug, anno=year
+                voce__slug=vo.slug, anno=self.year
             ).values(
                 'territorio__istat_id', 'territorio__slug', 'valore_procapite'
             )
@@ -122,18 +132,39 @@ class Command(BaseCommand):
                 t_id = v['territorio__istat_id']
                 t_slug = v['territorio__slug']
                 t_url = "{0}/bilanci/{1}?year={2}".format(
-                    site_url, t_slug, year
+                    site_url, t_slug, self.year
                 )
                 val = v['valore_procapite']
+
+                # creates location record if not found
+                # with URL and empty values dictionary
+                # should not happen, as the're already
+                # there, created by the previous loop
+                # on indicatori
                 if t_id not in indicatori_voci_year:
                     indicatori_voci_year[t_id] = {'url': t_url, 'values': {}}
+
+                # adds value for voice
+                # voice ID is prefixed with _v
+                # to avoid clashes with indicatori values
                 indicatori_voci_year[t_id]['values']['v_' + str(vo.id)] = val
 
+        # csv output
         self.logger.debug(
             " writing output to map_{0}.csv".format(self.year)
         )
         with open("map_{0}.csv".format(self.year), 'wb') as csvfile:
+            # define csvkit writer
             writer = csvkit.py2.CSVKitWriter(csvfile, delimiter=";")
+
+            # write header row
+            writer.writerow(
+                ["istat_id", "url"] +
+                [i.denominazione for i in indicatori] +
+                [vo.denominazione for vo in voices]
+            )
+
+            # write data rows
             for t_id, iv in list(indicatori_voci_year.items()):
                 writer.writerow(
                     [t_id, iv['url']] +
